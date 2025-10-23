88befeb5ed3c8903130854a9f797378ebb2658e0
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-10-23 17:25:57 +0200

    initial app strucutre

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,12 @@
-import * as THREE from 'three';
-import { WebGPURenderer } from 'three/webgpu';
 import './style.css';
 
-async function init() {
-  if (!navigator.gpu) {
-    document.body.innerHTML = "<h2 style='color:white;text-align:center'>WebGPU not supported 😢</h2>";
-    return;
-  }
+const apiSelector = document.getElementById('api-selector') as HTMLSelectElement;
+const sceneSelector = document.getElementById('scene-selector') as HTMLSelectElement;
+const confirmButton = document.getElementById('confirm-button') as HTMLButtonElement;
 
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 100);
-  camera.position.z = 3;
+const benchmarkData = [];
 
-  const renderer = new WebGPURenderer({ antialias: true });
-  await renderer.init();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+confirmButton?.addEventListener('click', () => {
+  const selectedScene = sceneSelector.value;
 
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshStandardMaterial({ color: 0x44aa88 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-
-  const light = new THREE.DirectionalLight(0xffffff, 1);
-  light.position.set(3, 3, 3);
-  scene.add(light);
-
-  window.addEventListener("resize", () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-  });
-
-  function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
-  }
-
-  animate();
-}
-
-init();
+})
