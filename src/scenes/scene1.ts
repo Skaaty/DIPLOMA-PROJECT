c73b712c2cd0b0418d480998727172ee5a7cca00
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-10-25 17:57:11 +0200

    Switch to normalized material and delete lighting

--- src/scenes/scene1.ts
+++ src/scenes/scene1.ts
@@ -16,31 +16,6 @@ function initGeometries(): THREE.BufferGeometry[] {
     return geometries;
 }
 
-// function initLights(scene: THREE.Scene): void {
-//     const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
-//     scene.add(ambientLight);
-
-//     const directionalLight = new THREE.DirectionalLight(0xffffff, 1.2);
-//     directionalLight.position.set(10, 15, 10);
-//     directionalLight.castShadow = true;
-
-//     directionalLight.shadow.mapSize.width = 1024;
-//     directionalLight.shadow.mapSize.height = 1024;
-//     directionalLight.shadow.camera.near = 0.5;
-//     directionalLight.shadow.camera.far = 50;
-//     directionalLight.shadow.camera.left = -20;
-//     directionalLight.shadow.camera.right = 20;
-//     directionalLight.shadow.camera.top = 20;
-//     directionalLight.shadow.camera.bottom = -20;
-
-//     scene.add(directionalLight);
-
-//     const pointLight = new THREE.PointLight(0xffaa33, 0.8, 100);
-//     pointLight.position.set(-8, 8, -8);
-//     scene.add(pointLight);
-// }
-
-
 function initMeshes(
     scene: THREE.Scene,
     geometries: THREE.BufferGeometry[],
