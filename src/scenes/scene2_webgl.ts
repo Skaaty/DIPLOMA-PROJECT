02fdbc410a3941d920f86b8f3a48803a0be9aa83
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-11-04 22:55:21 +0100

    Setup scene, camera, renderer

--- src/scenes/scene2_webgl.ts
+++ src/scenes/scene2_webgl.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import type Stats from 'stats-gl';
 import { HDRLoader } from 'three/examples/jsm/loaders/HDRLoader.js';
 
 import { createStopButton, removeStopButton } from '../ui/benchmarkControls';
@@ -9,3 +10,34 @@ let camera: THREE.Camera;
 let renderer: THREE.WebGLRenderer;
 let material: THREE.Material;
 let geometries: THREE.BufferGeometry[];
+
+export async function initScene2Webgl(stats: Stats, onComplete: () => void): Promise<void> {
+    const oldCanvas = document.getElementById('my-canvas');
+
+    if (oldCanvas && oldCanvas.parentNode) {
+        oldCanvas.parentNode.removeChild(oldCanvas);
+    }
+
+    const canvas = document.createElement('canvas');
+    canvas.id = 'my-canvas';
+    document.body.appendChild(canvas);
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color('#000000')
+
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100)
+    camera.position.set(0, 0, 4);
+
+    renderer = new THREE.WebGLRenderer({
+        canvas,
+        antialias: true,
+        stencil: false,
+        depth: true,
+        alpha: true,
+        powerPreference: 'high-performance',
+    })
+
+    const hdrLoader = new HDRLoader();
+    const pmrem = new THREE.PMREMGenerator(renderer);
+
+}
